00ca543cb9d76a735aba78d0b190ed6598f99bd3
Alex Diwa <atdiwa@gmail.com>
2019-03-19 16:28:57 +1100

    modified landing page and nav js

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,11 @@
 function myScrollFunction(item) {
   var elmnt = document.querySelector(item);
   elmnt.scrollIntoView({block: 'start', behavior: 'smooth'});
+  var nav = document.querySelector('.nav-links');
+  nav.classList = "nav-links";
 }
 
 function myNavFunction() {
   var element = document.querySelector('.nav-links');
   element.classList.toggle("visible");
 }
-
-// $(document).ready(function() {
-
-//   $('#navbar').click(function() {
-//     $('.nav-links').toggleClass('visible');
-//   });
-
-//   $('.work').click(function() {
-//     $(window).scrollTo('.nav-work', {duration:500});
-//     $('.nav-links').toggleClass('visible');
-//   });
-
-//   $('.about').click(function() {
-//     $(window).scrollTo('.nav-about', {duration:500});
-//     $('.nav-links').toggleClass('visible');
-//   });
-
-//   $('.work-2').click(function() {
-//     $(window).scrollTo('.nav-work', {duration:500});
-//   });
-
-//   $('.about-2').click(function() {
-//     $(window).scrollTo('.nav-about', {duration:500});
-//   });
-  
-//   $('.scrolltop').click(function() {
-//     $(window).scrollTo((0, 0), {duration:500});
-//   });
-
-
-//   window.onscroll = function() {myFunction()};
-
-//   var navbar = document.getElementById("navbar");
-//   var sticky = navbar.offsetTop;
-
-//   function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//       navbar.classList.add("sticky");
-//     } else {
-//       navbar.classList.remove("sticky");
-//     }
-//   }
-
-// });
