2851a1f379390c7cb30b6867d8df792032bae9fe
Alex Diwa <atdiwa@gmail.com>
2019-03-20 14:17:41 +1100

    toggle portfolio javascript completed and p5js sketch images added

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,29 @@ function myNavFunction() {
   var element = document.querySelector('.nav-links');
   element.classList.toggle("visible");
 }
+
+function myFolioFunction(classname) {
+
+
+  var element = document.querySelector(classname);
+  element.classList.toggle("visible");
+
+
+  
+}
+
+var imgState = {};
+function imgToggle(id) {
+    if (document.getElementById) {
+        var imgid = document.getElementById(id);
+        imgState[id] = (imgState[id]) ? false : true;
+
+        for (var img in imgState){
+            if (imgState[img] && img != id){
+                document.getElementById(img).style.display = 'none';
+                imgState[img] = false;
+            }
+        }
+        imgid.style.display = (imgid.style.display == 'block' ? 'none' : 'block');
+    }
+}
