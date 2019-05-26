d1fcce2bd21bf18579ae32bf1d9a7808f5597190
Alex <atdiwa@gmail.com>
2019-05-26 21:55:00 +1000

    adding styles and js toggle for dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,24 @@ function showWork(projectName) {
   return false;
 }
 
+function toggleDarkLight() {
+  const body = document.querySelector('body');
+  const moon = document.getElementById('moon');
+  
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+    moon.classList.remove('far');
+    moon.classList.add('fas');
+  }
+  else {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+    moon.classList.remove('fas');
+    moon.classList.add('far');
+  }
+}
+
 // accessibility for tabbing: https://hackernoon.com/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2
 
 function handleFirstTab(e) {
